0e4bb9ae06988b8787b1e63e44af2da0f570992f
M Matyi <mmatyi1999@gmail.com>
2021-02-19 14:23:21 +0000

    v1.0.3 - author

--- dist/bga.user.js
+++ dist/bga.user.js
@@ -2,7 +2,8 @@
 // @name           BGA Shortcuts
 // @namespace      http://github.com/MatyiFKBT
 // @description    Shortcuts for BGA
-// @version        1.0.0
+// @version        1.0.3
+// @author         MatyiFKBT
 // @downloadURL    https://github.com/MatyiFKBT/bga/raw/master/dist/bga.user.js
 // @include        https://boardgamearena.com/*
 // ==/UserScript==
